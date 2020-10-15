6fcfa17992eec66833f1551337cde4f575626b9e
root <root@9a17f8c21916>
2020-10-15 01:30:56 +0000

    Directory Ideration ip

--- src/_04_Directory_Iteration/DirectoryIterator.java
+++ src/_04_Directory_Iteration/DirectoryIterator.java
@@ -1,26 +1,35 @@
 package _04_Directory_Iteration;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import javax.swing.JFileChooser;
 
 public class DirectoryIterator {
 	public static void main(String[] args) {
+		File folderName;
+		File[] files;
 		/* 
 		 * The following is an example of how to list all of the files in a directory.
 		 * Once the program is running, the directory is chosen using the JFileChooser.
 		 */
 		JFileChooser jfc = new JFileChooser();
-		jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
 		int returnVal = jfc.showOpenDialog(null);
 		if (returnVal == JFileChooser.APPROVE_OPTION) {
-			File directory = jfc.getSelectedFile();
-			File[] files = directory.listFiles();
-			if(files != null) {
-				for(File f : files) {
-				  System.out.println(f.getAbsolutePath());
-				}
-			}
+			folderName = jfc.getCurrentDirectory();
+			files = folderName.listFiles();
+			System.out.println(folderName);
+		}
+		for(int i = 0; i < files.length;i++) {
+			
+		}
+		try {
+				FileWriter jfw = new FileWriter(folderName);
+				jfw.write("\n //copyright © 2020 Samson Kempiak");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
 		/*
