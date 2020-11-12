a05153487d599d85981c430bb04db444c8f24487
root <root@358fe0b2ad94>
2020-11-12 02:29:17 +0000

    working on lvl 5 module 0 (dont forget to ask him what the issue was with loading or saving)

---
+++ src/_06_Pixel_Art_Save_State/PixelArtMaker.java
@@ -0,0 +1,111 @@
+package _06_Pixel_Art_Save_State;
+
+import java.awt.FlowLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+import _05_Serialization.SaveData;
+import _06_Pixel_Art_Save_State.ColorSelectionPanel;
+
+public class PixelArtMaker implements MouseListener {
+	private JFrame window;
+	private GridInputPanel gip;
+	private GridPanel gp;
+	ColorSelectionPanel csp;
+	private JButton saveButton = new JButton("SAVEEEE");
+	private static final String DATA_FILE = "src/_06_Pixel_Art_Save_State/saved.dat";
+	
+	public void start() {
+		gip = new GridInputPanel(this);	
+		window = new JFrame("Pixel Art");
+		window.setLayout(new FlowLayout());
+		window.setResizable(false);
+		
+		window.add(gip);
+		window.add(saveButton);
+		window.pack();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
+	}
+
+	public void submitGridData(int w, int h, int r, int c) {
+		GridPanel grid = load();
+		if(grid != null) {
+			gp = grid;
+		} else {
+			// TODO: handle exception
+			gp = new GridPanel(w, h, r, c);
+		}
+		csp = new ColorSelectionPanel();
+		window.remove(gip);
+		window.add(gp);
+		window.add(csp);
+		gp.repaint();
+		gp.addMouseListener(this);
+		window.pack();
+	}
+	
+	public static void main(String[] args) {
+		new PixelArtMaker().start();
+	}
+	
+	private static GridPanel load() {
+		try (FileInputStream fis = new FileInputStream(new File(DATA_FILE)); ObjectInputStream ois = new ObjectInputStream(fis)) {
+			return (GridPanel) ois.readObject();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		} catch (ClassNotFoundException e) {
+			// This can occur if the object we read from the file is not
+			// an instance of any recognized class
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		if (e.getSource().equals(gp)) {
+			gp.setColor(csp.getSelectedColor());
+			System.out.println(csp.getSelectedColor());
+			gp.clickPixel(e.getX(), e.getY());
+			gp.repaint();
+		} else if(e.getSource().equals(saveButton)) {
+			save(gp);
+		}
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+	}
+	
+	private static void save(GridPanel data) {
+		try (FileOutputStream fos = new FileOutputStream(new File(DATA_FILE)); ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+			oos.writeObject(data);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
